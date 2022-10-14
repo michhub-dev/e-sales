76a7dde54dcb2ca6070a7688b701fe074f763cef
Michelle David <rachaelholy@gmail.com>
2022-10-14 03:05:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import  Header  from './components/Header'
+
+export default function App(){
+  return(
+    <>
+      <Header />
+    </>
+  )
+}
+  
